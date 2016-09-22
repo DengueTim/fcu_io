73cb6e99dc49a8c4ac517ae505fcebd6f72a37ea
Tim P <timothy.e.potter@gmail.com>
2016-09-22 11:10:19 +0200

    Added blackbox parser src dir. from cleanflight/blackbox-tools. Everything totally broken.

---
+++ src/blackbox/imu.h
@@ -0,0 +1,28 @@
+#ifndef IMU_H_
+#define IMU_H_
+
+typedef struct fp_vector {
+    float X;
+    float Y;
+    float Z;
+} t_fp_vector_def;
+
+typedef union {
+    float A[3];
+    t_fp_vector_def V;
+} t_fp_vector;
+
+// Angles in radians:
+typedef struct attitude_t {
+    float roll;
+    float pitch;
+    float heading;
+} attitude_t;
+
+void imuInit(void);
+void imuSetMagneticDeclination(double declination);
+
+void updateEstimatedAttitude(int16_t gyroADC[3], int16_t accSmooth[3], int16_t magADC[3], uint32_t currentTime, uint16_t acc_1G, float gyroScale, attitude_t *attitude);
+t_fp_vector calculateAccelerationInEarthFrame(int16_t accSmooth[3], attitude_t *attitude, uint16_t acc_1G);
+
+#endif
